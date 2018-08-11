3640c9708536ded815774b4529184671c6b6386c
Chris Poppell <clpoppell@valdosta.edu>
2018-08-11 10:07:58 -0400

    Added New Character Form

---
+++ app/src/main/java/com/tadbolmont/tabletopdeveloper/NewCharacterForm.java
@@ -0,0 +1,13 @@
+package com.tadbolmont.tabletopdeveloper;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewCharacterForm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_character_form);
+    }
+}
